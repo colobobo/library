8aac09818ff7d5430dc026ec1d19148ab00e61ca
Alexandre Massé <alexandre.masse94@gmail.com>
2020-04-01 17:46:09 +0200

    :label: add PayloadSocketError

--- src/payloads/index.ts
+++ src/payloads/index.ts
@@ -1,3 +1,8 @@
 export type PayloadSocket<T> = {data: T}
 
+export type PayloadSocketError<T = null> = PayloadSocket<T> & {
+  code: number,
+  message: string
+}
+
 export { PayloadsRoom } from './room'
