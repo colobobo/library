b79689516353aecc5247b35300abba360faea7cb
Eronne <erwann.letue@gmail.com>
2020-05-12 18:11:10 +0200

    :label: Remove unused properties

--- src/payloads/game.ts
+++ src/payloads/game.ts
@@ -2,8 +2,6 @@ import { PayloadSocket } from './index';
 
 export module PayloadsGame {
   export type Tick = PayloadSocket<{
-    x: number;
-    y: number;
     tick: number;
     objects: { [id: string]: { x: number; y: number } };
   }>;
