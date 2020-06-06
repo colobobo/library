02a9c75de418fefe89e52efee29af79d36fd8e77
Eronne <erwann.letue@gmail.com>
2020-06-06 15:32:00 +0200

    :label: Add types for game disposition

--- src/events/game.ts
+++ src/events/game.ts
@@ -2,6 +2,8 @@ export enum EventsGame {
   start = 'game:start',
   startSuccess = 'game:start:success',
   startError = 'game:start:error',
+  dispositionSelected = 'game:disposition:selected',
+  dispositionValidated = 'game:disposition:validated',
   sceneTypeUpdate = 'game:scene-type:update',
   statusUpdate = 'game:status:update',
   statusUpdateSuccess = 'game:status:update:success',
--- src/payloads/game.ts
+++ src/payloads/game.ts
@@ -1,11 +1,17 @@
 import { PayloadSocket, PayloadSocketError } from './index';
-import { scene } from '../enums';
+import { game, scene } from '../enums';
 
 export module PayloadsGame {
   export type StartSuccess = null;
   export type StartError = PayloadSocketError;
+  export type DispositionSelected = PayloadSocket<{
+    disposition: game.Disposition
+  }>;
+  export type DispositionValidated = PayloadSocket<{
+    disposition: game.Disposition
+  }>;
   export type SceneTypeUpdate = PayloadSocket<{
-    type: scene.Type;
+    type: scene.Type
   }>;
   export type End = {};
 }
