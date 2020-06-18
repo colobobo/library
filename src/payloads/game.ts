45005359de3eecbfc529e33bc3781013a2a96676
Eronne <erwann.letue@gmail.com>
2020-06-18 22:13:43 +0200

    :label: Add lives into game StartSuccess payload

--- src/payloads/game.ts
+++ src/payloads/game.ts
@@ -2,7 +2,7 @@ import { PayloadSocket, PayloadSocketError } from './index';
 import { game, scene } from '../enums';
 
 export module PayloadsGame {
-  export type StartSuccess = null;
+  export type StartSuccess = PayloadSocket<{ lives: number }>;
   export type StartError = PayloadSocketError;
   export type DispositionSelected = { disposition: game.Disposition };
   export type DispositionValidated = PayloadSocket<{ disposition: game.Disposition }>;
