3bcb8bf277f451ef2771b73afb6654718fb242ec
Eronne <erwann.letue@gmail.com>
2020-06-20 21:13:03 +0200

    :label: Add round score details

--- src/types/round.ts
+++ src/types/round.ts
@@ -18,13 +18,15 @@ export type ScoreDetail = {
 
 export type ScoreDetails = {
   total: number;
-  arrivedMembers: ScoreDetail;
-  traps: ScoreDetail;
-  remainingTime?: ScoreDetail;
+  details: {
+    arrivedMembers: ScoreDetail;
+    traps: ScoreDetail;
+    remainingTime?: ScoreDetail;
+  }
 }
 
 export type EndInformation = Information & {
   endType: enums.round.EndType;
   failCause: enums.round.FailCauses | null;
-  roundScoreDetails: ScoreDetails;
+  roundScore: ScoreDetails;
 }
